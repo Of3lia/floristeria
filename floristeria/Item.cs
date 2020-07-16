891248692062c7a127767eec1f1897cc7e56e122
Ofelia <ofeliacocucci@outlook.com>
2020-07-16 10:49:04 +0200

    1.1

---
+++ floristeria/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Item
+{
+	public Item(float price)
+	{
+		this.Price = price;
+	}
+	protected float Price { get; set; }
+}
---
+++ floristeria/Tree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Tree : Item
+{
+	public Tree(float price, float height): base(price)
+	{
+		this.Height = height;
+	}
+
+	private float Height { get; set; }
+}
