64b46c0eb4408ff2c261dc9ee05d0d1c0d9800ea
Ofelia <ofeliacocucci@outlook.com>
2020-07-20 11:11:41 +0200

    2.1

---
+++ WPF_floristeria/Florist_WPF/Florist_WPF/Models/FlowerShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Florist_WPF.Models
+{
+    class FlowerShop
+    {
+        public FlowerShop(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
